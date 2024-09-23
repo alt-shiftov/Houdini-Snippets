5a15d14f113333e895dc0bc80935a13b694bd054
Danil Rudenko <147479956+alt-shiftov@users.noreply.github.com>
2024-09-24 00:37:20 +0800

    update resample functions, add more comments, make clear examples

--- VEX/resample_functions.h
+++ VEX/resample_functions.h
@@ -1,7 +1,21 @@
-// ### Resample by length - resamplebylength()
-// Функция для ресампла линии. с pin points
-// Работает не совсем корректно, если много линий, то может хуйню выдавать. 
+// resamplebylength() - resample function, result is similar to the Resample SOP.
+// The simple version generates the @sourceprim and @sourceprimuv attributes, 
+// which are useful for interpolating attributes from the original curve to the new resampled points.
 
+// resamplebylength_pin() - This version allows resampling a curve while keeping certain points fixed (pinned).
+// Pinned points are defined by an integer array of point indices (pinpts[]).
+// Pinned points will remain in place during resampling.
+
+// uv01 - starting UV parameter on the curve from which the resampling begins.
+// uv02 - ending UV parameter on the curve where the resampling stops.
+// prim - primitive curve number.
+// minlen - minimum segment length
+// newprim - output primitive number
+// newpts[] - output array of new point indices
+// uvs[] - output UV coordinates of new points
+
+
+// BASE function
 function void resamplebylength(int geo; float uv01, uv02; int prim; float minlen; int newprim; int newpts[]; float uvs[]){
     int divs = 4;
     float dist = primarclen(geo, set(uv01, 0), set(uv02, 0), prim, divs, PRIMUV_UNIT_TO_REAL);
@@ -20,41 +34,60 @@ function void resamplebylength(int geo; float uv01, uv02; int prim; float minlen
     }
 }
 
-// VECTOR2 UVW
 
-function void resamplebylength(int geo; vector2 uvw; int prim; float minlen; int newprim; int newpts[]; float newuvs[]){
-    resamplebylength(geo, uvw[0], uvw[1], prim, minlen, newprim, newpts, newuvs);
-}
-
-// RESAMPLE WITH PIN POINTS
-function void resamplebylength(int geo; float uv01, uv02; int prim; float minlen; int newprim; int pinpts[]; float pinuvs[]; int newpts[]; float newuvs[]){
+// RESAMPLE WITH PIN UVs. VOID
+function void resamplebylength_pin(int geo; float uv01, uv02; int prim; float minlen; int newprim; float pinuvs[]; int newpts[]; float newuvs[]){
     // before pin points
     resamplebylength(geo, uv01, pinuvs[0], prim, minlen, newprim, newpts, newuvs);
 
     // between pin points
-    for(int i = 0; i < len(pinpts)-1; i++){
-        int nexti = (i+1) % len(pinpts);
+    for(int i = 0; i < len(pinuvs)-1; i++){
+        int nexti = (i+1) % len(pinuvs);
         resamplebylength(geo, pinuvs[i], pinuvs[nexti], prim, minlen, newprim, newpts, newuvs);
     }
     // after pin points
     resamplebylength(geo, pinuvs[-1], uv02, prim, minlen, newprim, newpts, newuvs);
 }
 
-// RETURN NEW PRIM (WITH PIN POINTS)
-function int resamplebylength(int geo; float uv01, uv02; int prim; float minlen; int pinpts[]; float pinuvs[]; int newpts[]; float newuvs[]){
+// RESAMPLE WITH PIN UVs. RETURN NEW PRIM
+function int resamplebylength_pin(int geo; float uv01, uv02; int prim; float minlen; float pinuvs[]; int newpts[]; float newuvs[]){
     int newprim = addprim(geo, 'polyline');
-    resamplebylength(geo, uv01, uv02, prim, minlen, newprim, pinpts, pinuvs, newpts, newuvs);
+    resamplebylength_pin(geo, uv01, uv02, prim, minlen, newprim, pinuvs, newpts, newuvs);
     return newprim;
 }
 
-function int resamplebylength(int geo; vector2 uvw; int prim; float minlen; int pinpts[]; float pinuvs[]; int newpts[]; float newuvs[]){
+// get vertex by prim and point
+function int pointprimvertex(int geo; int ptnum; int primnum){
+    int vtxs01[] = pointvertices(geo, ptnum);
+    int vtxs02[] = primvertices(geo, primnum);
+
+    foreach(int vtx; vtxs01){
+        if (find(vtxs02, vtx) >= 0){
+            return vtx;
+        }
+    }
+    return -1;
+}
+
+// RESAMPLE WITH PIN POINTS. RETURN NEW PRIM
+function int resamplebylength_pin(int geo; float uv01, uv02; int prim; float minlen; int pinpts[]; int newpts[]; float newuvs[]){
     int newprim = addprim(geo, 'polyline');
-    resamplebylength(geo, uvw[0], uvw[1], prim, minlen, newprim, pinpts, pinuvs, newpts, newuvs);
+
+    float pinuvs[];
+    foreach(int pt; pinpts){
+        int vtx = pointprimvertex(geo, pt, prim);
+        float uv = vertexcurveparam(geo, vtx);
+        append(pinuvs, uv);
+    }
+
+    resamplebylength_pin(geo, uv01, uv02, prim, minlen, newprim, pinuvs, newpts, newuvs);
     return newprim;
 }
 
 
-function void setuvtopoints(int geo; int pts[]; int primnum; float uvs[]; string primchannel; string uvchannel){
+// This function assigns the @sourceprim and @sourceprimuvw attributes to the newly generated points.
+// It is useful for transferring attributes from the original primitive to the new one.
+function void setuvtopoints(int geo; int pts[]; int primnum; float uvs[]; string primchannel, uvchannel){
     foreach(int idx; int pt; pts){
         float u = uvs[idx];
         setpointattrib(geo, primchannel, pt, primnum);
@@ -62,19 +95,7 @@ function void setuvtopoints(int geo; int pts[]; int primnum; float uvs[]; string
     }
 }
 
-
-// RETURN NEW PRIM
-function int resamplebylength(int geo; float uv01, uv02; int prim; float minlen; int newpts[]; float newuvs[]){
-    int newprim = addprim(geo, 'polyline');
-    resamplebylength(geo, uv01, uv02, prim, minlen, newprim, newpts, newuvs);
-    return newprim;
-}
-
-function int resamplebylength(int geo; vector2 uvw; int prim; float minlen; int newpts[]; float newuvs[]){
-    int newprim = addprim(geo, 'polyline');
-    resamplebylength(geo, uvw[0], uvw[1], prim, minlen, newprim, newpts, newuvs);
-    return newprim;
-}
+///////////////////////////////////////////////////////////////////////////////////
 
 // SIMPLE VERSION OF RESAMPLE
 function int resamplebylength(int geo; int prim; float minlen){
@@ -83,5 +104,27 @@ function int resamplebylength(int geo; int prim; float minlen){
     float newuvs[] = array();
     resamplebylength(geo, 0.0, 1.0, prim, minlen, newprim, newpts, newuvs);
     setuvtopoints(geo, newpts, prim, newuvs, 'sourceprim', 'sourceprimuv');
+    
     return newprim;
+}
+
+
+// SIMPLE VERSION OF RESAMPLE WITH PIN POINTS
+function int resamplebylength_pin(int geo; float uv01, uv02; int prim; float minlen; int pinpts[]){
+    int newprim = addprim(geo, 'polyline');
+
+    float pinuvs[]; 
+    int newpts[];
+    float newuvs[];
+    foreach(int pt; pinpts){
+        int vtx = pointprimvertex(geo, pt, prim);
+        float uv = vertexcurveparam(geo, vtx);
+        append(pinuvs, uv);
+    }
+
+    resamplebylength_pin(geo, uv01, uv02, prim, minlen, pinuvs, newpts, newuvs);
+    setuvtopoints(geo, newpts, prim, newuvs, 'sourceprim', 'sourceprimuv');
+
+    return newprim;
+
 }
